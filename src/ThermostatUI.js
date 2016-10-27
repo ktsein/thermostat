a0a001a056a328f2e9b1ce766d6a111489f679ef
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-10-27 16:11:48 +0100

    Add openweather data to show temperature of cities

--- src/ThermostatUI.js
+++ src/ThermostatUI.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
 
   var thermostat = new Thermostat();
+  var api_key = "90e9089cddc68f01cf03c6169b4b0ed5";
   updateTemperature();
+
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + "London" + "&units=metric&APPID=" + api_key,function(result){
+    $("#outside_temperature").text(result.main.temp + " ˚C");
+  });
+
+  $("#citylist").change(function(){
+    var cityName = $("#citylist").val();
+    console.log(cityName);
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&APPID=" + api_key,function(result){
+      $("#outside_temperature").text(result.main.temp + " ˚C");
+    });
+  });
   $("#down").click(function (){
     thermostat.down();
     updateTemperature();
