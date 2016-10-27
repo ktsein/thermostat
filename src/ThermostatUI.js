9ee7722ccadf45117cf31706ba1fa9c01d0998dd
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-10-27 13:42:49 +0100

    all buttons working

--- src/ThermostatUI.js
+++ src/ThermostatUI.js
@@ -10,5 +10,18 @@ $(document).ready(function(){
     thermostat.up();
     $("#temperature").text(thermostat.temperature + " ˚C");
   });
-
+  $("#reset").click(function (){
+    thermostat.reset();
+    $("#temperature").text(thermostat.temperature + " ˚C");
+  });
+  $("#powersaverOn").click(function (){
+    thermostat.setPowersaverOn();
+    $("#temperature").text(thermostat.temperature + " ˚C");
+    $("#pwsStatus").text("On");
+  });
+  $("#powersaverOff").click(function (){
+    thermostat.setPowersaverOff();
+    $("#temperature").text(thermostat.temperature + " ˚C");
+    $("#pwsStatus").text("Off");
+  });
 });
